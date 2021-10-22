a58c808fca58b98c2087d9828ea3909ffbab4e2b
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2021-10-22 16:11:58 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,11 @@
+famous_person = "Alec Baldwin"
+is_active = true
+famous_person = "Billy Baldwin"
+num_3 = 15
+num_9 = 20
+sum = num_3 + num_9
+animals = ['zebra','lion','rhinoceros']
+animals.delete_at(0)
+animals.shift
+famous_person.length
+famous_person.size
