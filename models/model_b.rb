8572877452c06f22c26181af276f4e1ce83e1e9e
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2021-10-22 16:31:00 -0400

    input vehicle attributes

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+vehicle = {
+
+name: 'BMW',
+model: '540i',
+"wheels" => 4,
+"speakers" => 16,
+"is_fast" => true,
+"is_old" => false,
+"best_features" => ['infotainment','response time','handling'],
+"upgrade_features" => ['soft close doors','22 in wheels']
+
+}
