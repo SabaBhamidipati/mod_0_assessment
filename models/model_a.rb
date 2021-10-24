3cce68e1a9518d00f2a986cca59cb6ae2cd78285
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2021-10-24 18:44:31 -0400

    update hash syntax

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,12 +1,12 @@
 airport = {
 
-name: 'OHare',
-city_name: 'Chicago',
-"number_of_flights" => 500,
-"number_of_terminals" => 5,
-"is_active" => true,
-"is_busiest_airport" => false,
-"trains_to_airport" => ['blue','orange','pink'],
-"highways_to_airport" => ['95','275']
+  "name" => 'OHare',
+  "city_name" => 'Chicago',
+  "number_of_flights" => 500,
+  "number_of_terminals" => 5,
+  "is_active" => true,
+  "is_busiest_airport" => false,
+  "trains_to_airport" => ['blue','orange','pink'],
+  "highways_to_airport" => ['95','275']
 
 }
